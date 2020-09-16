32c6127fa1d681de3fe0d029667b2e53b0b3209c
wywang <727936199@qq.com>
2020-09-16 20:23:29 +0800

    安装chai.js

---
+++ src/icon.vue
@@ -0,0 +1,21 @@
+<template>
+  <svg aria-hidden="true">
+    <use :xlink:href=`#i-${name}`></use>
+  </svg>
+</template>
+
+<script lang="ts">
+export default{
+  props:['name']
+}
+</script>
+
+<style lang="scss" scoped>
+  .m-icon {
+    width: 1em;
+    height: 1em;
+    vertical-align: -0.15em;
+    fill: currentColor;
+    overflow: hidden;
+  }
+</style>
