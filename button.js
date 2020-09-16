e0ad9c99718e98aab2031c366fe8f6e28c081a02
wywang <727936199@qq.com>
2020-09-16 13:37:09 +0800

    加入button，将button.js组件改为单文件组件

--- button.js
+++ button.js
@@ -1,15 +1,2 @@
-Vue.component('moon-button',{
-  template:`
-    <button class="moon-button">按钮</button>
-  `
-})
 
-let request=new XMLHttpRequest();
-request.open('GET','a/b/c',true);
-request.onreadystatechange=()=>{
-  if(request.readyState===4&&request.status===200){
-    console.log(request.responseText);
-  }
-}
 
-request.send();
---
+++ src/button.vue
@@ -0,0 +1,28 @@
+<template>
+  <button class="moon-button">按钮</button>
+</template>
+
+<script lang="ts">
+
+</script>
+
+<style lang="scss" scoped>
+  .moon-button{
+    height:var(--button-height);
+    font-size: var(--font-size);
+    padding: 0 1em;
+    border-radius: var(--border-radius);
+    border:1px solid var(--border-color);
+    background: var(--button-bg);
+    &:hover{
+      border-color:var(--border-color-hover);
+    }
+    &:active{
+      background-color: var(--button-active-bg);
+    }
+    &:focus{
+      outline:none;
+    }
+  }
+
+</style>
