5bf708f57aa17ceedd6b12ff875a244389193419
wywang <727936199@qq.com>
2020-09-16 21:14:52 +0800

    使用chai.expect添加四个测试用例，第五个测试button的click时不会写测试用例

--- src/app.js
+++ src/app.js
@@ -14,3 +14,88 @@ new Vue({
     loading3:false,
   }
 })
+
+//单元测试
+import chai from 'chai'
+const expect=chai.expect
+//测试icon属性
+{
+  const Constructor=Vue.extend(Button);
+  const vm=new Constructor({
+    propsData:{
+      icon:"right",
+    }
+  })
+  vm.$mount()
+  let useElement=vm.$el.querySelector('use');
+  console.log(useElement);
+  expect(useElement.getAttribute('xlink:href')).to.eq('#i-right')
+  vm.$el.remove();
+  vm.$destroy();
+}
+//测试loading属性
+{
+  const Constructor=Vue.extend(Button);
+  const vm=new Constructor({
+    propsData:{
+      icon:"right",
+      loading:true
+    }
+  })
+  vm.$mount()
+  let useElement=vm.$el.querySelector('use');
+  const res=expect(useElement.getAttribute('xlink:href')).to.eq('#i-loading')
+  vm.$el.remove();
+  vm.$destroy();
+}
+//测试iconPosition属性
+{
+  const div=document.createElement('div');
+  document.body.appendChild(div);
+  const Constructor=Vue.extend(Button);
+  const vm=new Constructor({
+    propsData:{
+      icon:"right",
+      iconPosition:"right"
+    }
+  })
+  vm.$mount(div)
+  let svg=vm.$el.querySelector('svg');
+  expect(window.getComputedStyle(svg).order).to.eq('2')
+  vm.$el.remove();
+  vm.$destroy();
+}
+{
+  const div=document.createElement('div');
+  document.body.appendChild(div);
+  const Constructor=Vue.extend(Button);
+  const vm=new Constructor({
+    propsData:{
+      icon:"right",
+      iconPosition:"left"
+    }
+  })
+  vm.$mount(div)
+  let svg=vm.$el.querySelector('svg');
+  expect(window.getComputedStyle(svg).order).to.eq('1')
+  vm.$el.remove();
+  vm.$destroy();
+}
+
+//测试click
+{
+  const Constructor=Vue.extend(Button);
+  const vm=new Constructor({
+    propsData:{
+      icon:"right",
+    }
+  })
+  vm.$mount()
+  vm.$on('click',()=>{
+    console.log(1);
+  })
+  vm.$el.click()
+  // expect(useElement.getAttribute('xlink:href')).to.eq('#i-right')
+  vm.$el.remove();
+  vm.$destroy();
+}
